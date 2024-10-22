36de701715735d32bbec9a39da5f37dfa9bcf190
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-10-22 20:10:56 -0300

    Criado base

---
+++ backend/routes/routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+// const ***Controler = require("../controllers/***** */.js")
+
+// router.get('/', ***Controler.***) 
+
+module.exports = router
