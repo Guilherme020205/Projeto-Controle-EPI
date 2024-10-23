88a34ebcefab002b1a08516c46350d88b513fb47
Guilherme020205 <104910701+Guilherme020205@users.noreply.github.com>
2024-10-23 17:52:18 -0300

    Criado

---
+++ backend/models/epi.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const database = require('./db')
+
+const EPIS = database.define('epis', {
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = EPIS; 
