840f0b1764ba8979b0db6f11d87f472deb3221d9
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-10-30 20:13:22 -0300

    Senhor vai pfv

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,6 +5,12 @@ const funcionarioControler = require('../controllers/funcionarioController')
 const listaControler = require('../controllers/listaController')
 const loginController = require('../controllers/loginController')
 
+router.post('/epiController', epiControler.cadastrarEpi);
+router.get('/epiController', epiControler.listarEpi);
+router.put('/epiController/:id', epiControler.atualizarEpi);
+router.delete('/epiController/:id', epiControler.deletarEpi);
+
+
 router.get('/listarFuncionario', funcionarioControler.listarFuncionario) 
 router.post('/cadastrarFuncionario', funcionarioControler.cadastrarFuncionario)
 
