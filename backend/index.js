9fc3738410630715f5646d2625e8d6f6d8966894
PitDemon <vinicius.linhar1@gmail.com>
2024-10-29 21:54:00 -0300

    Config oks

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 (async () => {
     const database = require('./models/db')
-    const Epis = require('./models/epi')
+    const Epis = require('./models/TabelaEpis')
+    const Funcionario = require('./models/TabelaFuncionario')
+    const Login = require('./models/TabelaLogin.js')
+    const Pedidos = require('./models/TabelaPedidos')
+    const Setor = require('./models/TabelaSetor')
+    
     await database.sync();
     
-    const novoEPI = await Epis.create({
-        nome: "Luva"
-    })
+    const criarEpis = await Epis.create()
+    const criarFuncionario = await Funcionario.create()
+    const criarLogin = await Login.create()
+    const criarPedidos = await Pedidos.create()
+    const criarSetor = await Setor.create()
     console.log(novoEPI);
 
 
