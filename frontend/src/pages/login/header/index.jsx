7f312b49e7e4480dfb00ffb94e8ad1f75f336c78
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-11-07 21:06:27 -0300

    Feito o front

---
+++ frontend/src/pages/login/header/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export default function Header() {
+    return (
+        <>
+            <div className='header'>
+                <h1>EPI Control</h1>
+            </div>
+        </>
+    )
+}
--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react"
+import { CiLogin } from "react-icons/ci";
+import "./style.css"
+import Header from "./header/index.jsx";
 
-export default function PgLogin(){
+export default function PgLogin() {
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
     return (
         <>
-            <h1>Olá Mundo!</h1>
-            <p>Login</p>
+            <header>
+                <Header />
+            </header>
+            
+            <div className="box-principal">
+                <div className="box-form">
+                    <h2>Login no sistema</h2>
+                    <form action="">
+                        <p>email</p>
+                        <input
+                            type="email"
+                            placeholder="admin@gmail.com"
+                            value={email}
+                            name="email"
+                            required
+                        />
+
+                        <p>senha</p>
+                        <input
+                            type="password"
+                            placeholder="admin1"
+                            value={senha}
+                            name="password"
+                            required
+                        />
+
+                    </form>
+                </div>
+                <div className="box-button">
+                    <button>Login<CiLogin className="icon-CiLogin" /></button>
+                </div>
+            </div>
         </>
     )
 }
